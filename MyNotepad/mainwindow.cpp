98c887c85f5220d63aa5914607bff8d7a337ca87
cheying123 <2994554807@qq.com>
2024-10-28 16:06:11 +0800

    构建了界面布局

---
+++ MyNotepad/mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    statusLabel.setMaximumWidth(150);
+    statusLabel.setText("长度:" + QString::number(0) + "    行号: " + QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusLabel);
+
+    statusCursorLabel.setMaximumWidth(150);
+    statusCursorLabel.setText("行:" + QString::number(0) + "    列: " + QString::number(1));
+    ui->statusbar->addPermanentWidget(&statusCursorLabel);
+
+    QLabel *author = new QLabel(ui->statusbar);
+    author->setText(tr("黄文轩"));
+    ui->statusbar->addPermanentWidget(author);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
